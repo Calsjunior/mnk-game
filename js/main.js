8f064a4cc2c1abd7000083b5381a29667ea1c13f
cal <sakphea05@gmail.com>
2026-06-13 10:25:55 +0700

    style: format code to use 2 indent

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 const GameBoard = (() => {
-    const rows = 3;
-    const cols = 3;
-    const board = [];
+  const rows = 3;
+  const cols = 3;
+  const board = [];
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < cols; j++) {
-            board[i].push(0);
-        }
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push(0);
     }
+  }
 
-    const getBoard = () => board;
+  const getBoard = () => board;
 
-    const printBoard = () => {
-        console.table(board);
-    };
+  const printBoard = () => {
+    console.table(board);
+  };
 
-    return {
-        getBoard,
-        printBoard,
-    };
+  return {
+    getBoard,
+    printBoard,
+  };
 })();
 
 const GameController = (() => {
-    const board = GameBoard.getBoard();
+  const board = GameBoard.getBoard();
 
-    const playRound = (rows, cols) => {
-        board[rows][cols] = "x";
-    };
+  const playRound = (rows, cols) => {
+    board[rows][cols] = "x";
+  };
 
-    return {
-        init() {
-            playRound(1, 2);
-            GameBoard.printBoard();
-        },
-    };
+  return {
+    init() {
+      playRound(1, 2);
+      GameBoard.printBoard();
+    },
+  };
 })(GameBoard);
 
 GameController.init();
