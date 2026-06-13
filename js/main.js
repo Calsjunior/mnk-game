10a271aeb9012df17707b3ca92e9b80986c91048
cal <sakphea05@gmail.com>
2026-06-13 11:28:22 +0700

    refactor: create board using js Array object

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 const GameBoard = (() => {
   const rows = 3;
   const cols = 3;
-  const board = [];
-
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < cols; j++) {
-      board[i].push(0);
-    }
-  }
+  const board = Array(rows)
+    .fill()
+    .map(() => Array(cols).fill(0));
 
   const getBoard = () => board;
 
