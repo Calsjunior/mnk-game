cc3fbfbbcda4c7a20c038834e51e2a467c4c71a7
cal <sakphea05@gmail.com>
2026-06-22 12:03:49 +0700

    feat(constants): add screen enum

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,10 @@ const GAME_STATE = Object.freeze({
   DRAW: "draw",
 });
 
-export { GAME_STATE };
+const SCREENS = Object.freeze({
+  MENU: "menu",
+  GAME: "game",
+  SETTINGS: "settings",
+});
+
+export { GAME_STATE, SCREENS };
